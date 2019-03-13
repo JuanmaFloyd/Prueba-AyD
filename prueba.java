aef29614dd27930c7b98638b36fee35d7bb9d240
FNEBK <31111055+FNEBK@users.noreply.github.com>
2019-03-13 09:32:08 -0300

    Update prueba.java
    Version 2.0

--- prueba.java
+++ prueba.java
@@ -1,5 +1,5 @@
 public class Prueba{
     public static void maiun(String[] args){
-        System.out.println("STEFO SE LA COME DOBLADA!");
+        System.out.println("STEFO SE LA COME DOBLADA EN EL NUEVO!");
     }
 }
